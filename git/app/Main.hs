07313c9d0b0485cdae5bc7e7795abebe4ec6f08f
mrsekut <k.cloudspider@gmail.com>
2024-12-22 17:38:51 +0900

    uncomment

--- git/app/Main.hs
+++ git/app/Main.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments    #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Main (main) where
 
-import System.Directory (createDirectoryIfMissing)
-import System.FilePath ((</>))
-import System.IO (IOMode (WriteMode), hPutStrLn, withFile, hSetBuffering, stdout, stderr, BufferMode (NoBuffering))
+import           System.Directory (createDirectoryIfMissing)
+import           System.FilePath  ((</>))
+import           System.IO        (BufferMode (NoBuffering), IOMode (WriteMode),
+                                   hPutStrLn, hSetBuffering, stderr, stdout,
+                                   withFile)
 
 main :: IO ()
 main = do
@@ -14,13 +16,9 @@ main = do
     hSetBuffering stdout NoBuffering
     hSetBuffering stderr NoBuffering
 
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    hPutStrLn stderr "Logs from your program will appear here"
-
-    -- Uncomment this block to pass first stage
-    -- let createParents = True
-    -- createDirectoryIfMissing createParents ".git"
-    -- createDirectoryIfMissing createParents (".git" </> "objects")
-    -- createDirectoryIfMissing createParents (".git" </> "refs")
-    -- withFile (".git" </> "HEAD") WriteMode $ \f -> hPutStrLn f "ref: refs/heads/main"
-    -- putStrLn $ "Initialized git directory"
+    let createParents = True
+    createDirectoryIfMissing createParents ".git"
+    createDirectoryIfMissing createParents (".git" </> "objects")
+    createDirectoryIfMissing createParents (".git" </> "refs")
+    withFile (".git" </> "HEAD") WriteMode $ \f -> hPutStrLn f "ref: refs/heads/main"
+    putStrLn $ "Initialized git directory"
